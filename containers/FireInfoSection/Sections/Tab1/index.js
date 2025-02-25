bc8ecd287d74c3e4677f5ee8f4a3968f279f81e9
IB - Scott <scott@impossible-bureau.com>
2025-02-25 16:12:37 -0700

    add title to mobile tab 1

--- containers/FireInfoSection/Sections/Tab1/index.js
+++ containers/FireInfoSection/Sections/Tab1/index.js
@@ -7,23 +7,44 @@ import styles from "./style.module.scss";
 import SplitTextBg from "@/components/SplitTextBg";
 import { MediaWCaption } from "@/components/MediaWCaption";
 
+import useWindowSize from "@/hooks/useWindowSize";
+
 import { mapToGlobalProgress } from "../../utils";
 
 export default function Tab1({ index }) {
+  const { width } = useWindowSize();
+  const smScreen = width < 1080;
+  const sectionTitleRef = useRef(null);
   const titleRef = useRef(null);
 
   return (
     <div className={`${styles.container}`}>
-      <ST.Waypoint
-        at={mapToGlobalProgress(index, 1)}
-        onCall={() => titleRef.current?.restart()}
-        onReverseCall={() => titleRef.current?.reverse()}
-      />
+      <div className={styles.titleWrap}>
+        {smScreen && (
+          <>
+            <ST.Waypoint
+              at={mapToGlobalProgress(index, 1)}
+              onCall={() => sectionTitleRef.current?.restart()}
+              onReverseCall={() => sectionTitleRef.current?.reverse()}
+            />
+            <div className={`${styles.sectionTitle}`}>
+              <SplitTextBg ref={sectionTitleRef} color="cream" inline>
+                <h2>{CONTENT.sectionTitle}</h2>
+              </SplitTextBg>
+            </div>
+          </>
+        )}
+        <ST.Waypoint
+          at={mapToGlobalProgress(index, 1)}
+          onCall={() => titleRef.current?.restart()}
+          onReverseCall={() => titleRef.current?.reverse()}
+        />
 
-      <div className={`${styles.title}`}>
-        <SplitTextBg ref={titleRef} color="cream" inline>
-          <h2 className={styles.title}>{CONTENT.title}</h2>
-        </SplitTextBg>
+        <div className={`${styles.title}`}>
+          <SplitTextBg ref={titleRef} color="cream" inline>
+            <h2 className={styles.title}>{CONTENT.title}</h2>
+          </SplitTextBg>
+        </div>
       </div>
       <div className={styles.mediaWCaption}>
         <MediaWCaption url={CONTENT.image.src} caption={CONTENT.image.alt} />
@@ -33,6 +54,7 @@ export default function Tab1({ index }) {
 }
 
 const CONTENT = {
+  sectionTitle: "The State Of Our Forests",
   title:
     "But right now, many of our forests exhibit declining health and resilience",
   image: {
