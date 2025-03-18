18a02b56c2ea9f3d36c9c8003162f2fc08af2329
Mary E Henderson <lhenderson303@gmail.com>
2025-03-18 12:17:11 -0600

    add favicon files

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -5,6 +5,11 @@ import Layout from "@/containers/Layout";
 import { ModalProvider } from "@/context/ModalContext";
 import Footer from "@/components/Footer";
 import { client } from "@/sanity/lib/client";
+import Head from "next/head";
+import faviconIco from "../../public/favicon/favicon.ico";
+import favicon from "../../public/favicon/favicon.svg";
+import favicon96 from "../../public/favicon/favicon-96x96.png";
+import manifest from "../../public/static/webmanifest.json";
 
 import "@/styles/global.scss";
 
@@ -29,6 +34,31 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <Head>
+      <title>Colorado Mass Timber Coalition</title>
+        <meta name="description" content="Supporting healthy, resilient forests through a vibrant forest-products economy including mass timber in the Rocky Mountain Region" />
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="628" />
+        <meta property="og:title" content="Colorado Mass Timber Coalition" />
+        <meta property="og:site_name" content="Colorado Mass Timber Coalition" />
+        <meta
+          property="og:description"
+          content="Colorado Mass Timber Coalition supports healthy, resilient forests through a vibrant forest-products
+          economy including mass timber throughout the Rocky Moutain Region"
+        />
+        <meta property="og:url" content="https://www.colorado-mass-timber-coalition.com/" />
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, viewport-fit=cover"
+        />
+        <link rel="icon" type="image/png" href={favicon96.src} sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href={favicon.src} />
+        <link rel="shortcut icon" href={faviconIco.src} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="CMTC" />
+        <link rel="manifest" href={manifest.src} />
+      </Head>
       <body className={`${greedNarrow.variable} ${greedStandard.variable}`}>
         <ModalProvider>
           <Layout contactEmail={contactEmail} />
