c3e81fef428030ab2f83129de9a1e2474c3cbfde
IB - Scott <scott@impossible-bureau.com>
2025-03-19 09:46:50 -0600

    add footer animations

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,6 @@
 "use client";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
+import * as ST from "@bsmnt/scrollytelling";
 import FooterLogo from "../SVG/FooterLogo";
 
 import NewsletterSignup from "@/components/NewsletterSignup";
@@ -29,28 +30,88 @@ export default function Footer({ contactEmail }) {
   ];
 
   return (
-    <footer className={styles.footer}>
-      <div className={styles.content}>
-        <div className={styles.newsletter}>
-          <NewsletterSignup />
-        </div>
-        <div className={styles.socials}>
-          <div className={styles.stumpyWrap}>
-            <Stumpy type="plank" color="orange" />
+    <ST.Root start="top 80%" end="bottom bottom">
+      <footer className={styles.footer}>
+        <div className={styles.content}>
+          <ST.Animation
+            tween={{
+              start: 0,
+              end: 10,
+              fromTo: [
+                { opacity: 0, y: 30 },
+                { opacity: 1, y: 0 },
+              ],
+              duration: 0.6,
+              ease: "power2.out",
+            }}
+          >
+            <div className={styles.newsletter}>
+              <NewsletterSignup />
+            </div>
+          </ST.Animation>
+
+          <div className={styles.socials}>
+            <ST.Animation
+              tween={{
+                start: 20,
+                end: 30,
+                fromTo: [
+                  { opacity: 0, scale: 0.8 },
+                  { opacity: 1, scale: 1 },
+                ],
+                duration: 0.4,
+                ease: "back.out(1.7)",
+              }}
+            >
+              <div className={styles.stumpyWrap}>
+                <Stumpy type="plank" color="orange" />
+              </div>
+            </ST.Animation>
+
+            <div className={styles.socialWrap}>
+              {SOCIAL_LINKS.map((link, index) => (
+                <ST.Animation
+                  key={link.url}
+                  tween={{
+                    start: 30 + index * 0.2,
+                    end: 40 + index * 0.2 + 0.1,
+                    fromTo: [
+                      { opacity: 0, y: 20 },
+                      { opacity: 1, y: 0 },
+                    ],
+                    ease: "power2.out",
+                  }}
+                >
+                  <a
+                    href={link.url}
+                    className={styles.url}
+                    aria-label={link.ariaLabel}
+                    title={link.ariaLabel}
+                  >
+                    {link.icon}
+                  </a>
+                </ST.Animation>
+              ))}
+            </div>
           </div>
-          <div className={styles.socialWrap}>
-            {SOCIAL_LINKS.map((link) => (
-              <a href={link.url} key={link.url} className={styles.url} arai-label={link.ariaLabel} title={link.ariaLabel}>
-                {link.icon}
-              </a>
-            ))}
-          </div>
         </div>
-       
-      </div>
-      <div className={styles.logo}>
-        <FooterLogo />
-      </div>
-    </footer>
+
+        <ST.Animation
+          tween={{
+            start: 80,
+            end: 90,
+            fromTo: [
+              { opacity: 0, y: 20 },
+              { opacity: 1, y: 0 },
+            ],
+            ease: "power2.out",
+          }}
+        >
+          <div className={styles.logo}>
+            <FooterLogo />
+          </div>
+        </ST.Animation>
+      </footer>
+    </ST.Root>
   );
 }
